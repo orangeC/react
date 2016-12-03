e90a3c1ea17fa431d2800f8e20b8e493ae57cf7e
orangeC <18332563755@163.com>
2016-12-03 14:54:36 +0800

    modify data

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from "react-router"
 
 class BlogCard extends React.Component {
   render(){
     return (
         <div className="blog-card">
           <div className="blog-index"><span>{this.props.index}</span></div>
-          <div className="blog-content clearfix">
+          <div className="blog-content">
             <h3>{this.props.title}</h3>
             <p>{this.props.desc}</p>
-            <RaisedButton label="阅读更多" primary={true} />
+            <RaisedButton label="阅读更多" primary={true} href={`#/item/${this.props.url}`} />
           </div>
         </div>
       )
