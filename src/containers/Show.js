6bf75cbddde38a93baa805a223a2a734e644957b
orangeC <18332563755@163.com>
2017-01-15 18:13:39 +0800

    modify website

--- src/containers/Show.js
+++ src/containers/Show.js
@@ -26,13 +26,13 @@ const tilesData = [
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/1.png',
     title: ' ',
     author: '秀厨网（PC端项目页面）',
-    ref:"https://orangeC.github.io/"
+    ref:"http://orangeC.coding.me/showkitchen"
   },
   {
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/2.png',
     title: ' ',
     author: '迈思设计（jquery js插件）',
-    ref:"https://orangeC.github.io/maisi"
+    ref:"http://orangeC.coding.me/maisi"
   },
   {
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/5.png',
@@ -44,13 +44,13 @@ const tilesData = [
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/4.png',
     title: ' ',
     author: '第嘉（响应式）',
-    ref:"https://orangeC.github.io/digi"
+    ref:"http://orangeC.coding.me/digi"
   },
   {
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/3.png',
     title: ' ',
     author: '个人简历（移动端）',
-    ref:"https://orangeC.github.io/resume-swiper"
+    ref:"http://orangeC.coding.me/resume"
   },
   {
     img: 'https://raw.githubusercontent.com/orangeC/myImg/master/img/6.png',
@@ -68,7 +68,7 @@ const Show = () => (
     >
       <Subheader style={styles.word}>个人作品展示</Subheader>
       {tilesData.map((tile) => (
-        <a href={tile.ref} target="_blank"><GridTile
+        <a className="showA" href={tile.ref} target="_blank"><GridTile
           key={tile.img}
           title={tile.title}
           subtitle={<span><b>{tile.author}</b></span>}
