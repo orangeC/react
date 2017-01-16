9d22035b06b5dc6cc3c18ea4f2a9e01aaf815aba
orangeC <18332563755@163.com>
2017-01-16 09:24:44 +0800

    modify website

--- src/user/Game.js
+++ src/user/Game.js
@@ -12,7 +12,7 @@ class Game extends Component {
     return (
       <div className="gameRoot">
         <div style={styles.root}>
-          <a href="https://orangec.github.io/palette/" target="_blank"><RaisedButton label="五子棋"  /></a>
+          <a href="https://orangec.github.io/gobang/" target="_blank"><RaisedButton label="五子棋"  /></a>
           <a href="https://orangec.github.io/palette/" target="_blank"><RaisedButton label="你画我猜" primary={true}  /></a>
           <RaisedButton label="大鱼吃小鱼" secondary={true}  />
         </div>
